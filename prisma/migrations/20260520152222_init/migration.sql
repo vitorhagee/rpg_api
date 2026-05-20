3651187ed8744c2952fcc94345f8c82da500d92f
vitor hagee <vitorhagee@gmail.com>
2026-05-20 12:25:26 -0300

    configuração, gitignore,dependencias,readme,prisma e node configurados

---
+++ prisma/migrations/20260520152222_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Character" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "class" TEXT NOT NULL,
+    "height" INTEGER NOT NULL,
+    "race" TEXT NOT NULL
+);
